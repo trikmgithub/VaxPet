e8ad30df7d3b9c1521aa3fd7644c76b8c73ada85
Kiều Minh Trí <trikmforwork@gmail.com>
2025-08-08 11:18:15 +0700

    Forget password

---
+++ lib/domain/auth/usecases/forgot_password.dart
@@ -0,0 +1,13 @@
+import 'package:dartz/dartz.dart';
+import 'package:vaxpet/data/auth/models/verify_otp_req_params.dart';
+
+import '../../../core/usecase/usecase.dart';
+import '../../../service_locator.dart';
+import '../repositories/auth.dart';
+
+class ForgotPasswordUseCase extends UseCase<Either, VerifyOtpReqParams> {
+  @override
+  Future<Either> call({VerifyOtpReqParams? params}) async {
+    return await sl<AuthRepository>().verifyOtp(params!);
+  }
+}
